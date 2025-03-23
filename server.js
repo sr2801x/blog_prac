c9ecc60dbc841f37e3b28b26c832217b4a2b8721
rathoresneha <rathoresneha2801@gmail.com>
2025-03-23 19:41:12 +0530

    second commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-//const Users=require("./Model/Users");
+const Users=require("./Model/Users");
 const blog=require("./Model/blog");
 
 app.use(express.json());
@@ -10,16 +10,22 @@ app.use(express.urlencoded({ extended: true }));
 
 
 //adding user data into database every querry on db is asynchronous
-// app.post("/Users",async(req,res)=>{
-//   let {name,email, password} = req.body;
-//   let newUser=new Users({
-//     name:name,
-//     email:email,
-//     password:password
-//   });
-//   await newUser.save();
-//   res.send("user added")
-// })
+app.post("/Users",async(req,res)=>{
+  let {name,email, password} = req.body;
+  let newUser=new Users({
+    name:name,
+    email:email,
+    password:password
+  });
+  await newUser.save();
+  res.send("user added")
+})
+
+app.get("/Users",async(req,res)=>{
+  //1.read the data
+  let allusers=await Users.find();
+  res.send(allusers)
+})
 
 
 
@@ -32,6 +38,24 @@ app.post("/blog",async(req,res)=>{
   });
   await newblog.save();
   res.send("blog added")
+})
+
+app.get("/blogs",async(req,res)=>{
+  //1.read the data
+  let allblogs=await blog.find();
+  res.send(allblogs)
+})
+
+app.get("/Users/:id",async(req,res)=>{
+  let {id}=req.params;
+  let user=await Users.findById(id)
+  res.send(user)
+})
+
+app.get("/blogs/:id",async(req,res)=>{
+  let {id}=req.params;
+  let blogs=await blog.findById(id)
+  res.send(blogs)
 })
 
 
